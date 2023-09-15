97308a8320c3de013109caebb3fefab77d632aed
SayedS7 <smdase@hotmail.com>
2023-09-15 16:29:10 -0700

    g

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * dicription: print numbers of base16 in lowercase
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
